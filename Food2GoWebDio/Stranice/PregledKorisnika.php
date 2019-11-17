84cdeca4b313a2703d71da2000638c2c64bbac5f
igradiski <gradiski.zrinski.ivor@gmail.com>
2019-11-17 15:59:02 +0100

    Skripte za pregled i blokiranje korisnika u aplikaciji #35

---
+++ Food2GoWebDio/Stranice/PregledKorisnika.php
@@ -0,0 +1,28 @@
+<?php
+include("zaglavlje.php");
+include("../Backend/DohvacanjeKorisnika.php")
+?>
+
+
+<?php
+
+echo '<br/>';?>
+<br>
+
+    <p class="h2 text-center"> Upravljanje korisnicima </p><hr><br>
+
+        <form class="col-12" method="post" name="upravljanjeArtiklima" id="artikli" style="margin: 0 auto">
+           <?php TablicaKorisnika();
+            ?>
+
+            <p>0 je blokiran
+                <br>
+                1 je aktivan
+            </p>
+    </form>
+
+
+
+<?php
+include ('podnozje.php')
+?>
