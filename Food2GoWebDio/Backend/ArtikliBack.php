7f1a70aecf815be11c00cdfb7cf1befe4f3ef90e
sosko123 <hsostaric@foi.hr>
2019-11-17 16:45:46 +0100

    Php funkcija za dohvaćanje artikala iz baze podataka #36

--- Food2GoWebDio/Backend/ArtikliBack.php
+++ Food2GoWebDio/Backend/ArtikliBack.php
@@ -1,5 +1,44 @@
 <?php
 include ('../Baza/Baza.php');
+
+function DohvatiArtikle(){
+    $baza = new Baza();
+    $baza->spojiDB();
+    $artikli = $baza->selectDB("SELECT Artikl.ID,Artikl.Naziv,Artikl.Cijena,Artikl.KolicinaZaliha,Artikl.Opis,(SELECT Kategorija.Naziv FROM Kategorija WHERE Kategorija.ID=Artikl.KategorijaID) as kategorijaNaziv  FROM Artikl");
+    if($artikli->num_rows>0){
+
+        echo '<table id="table_id" class="display" border="1" style="width: 70%">
+                        <thead>
+                    <tr>
+                        <td>Naziv </td>
+                        <td>Cijena</td>
+                        <td>Količina</td>
+                        <td>Opis</td>
+                        <td>Naziv kategorije</td>
+                        <td>Ažuriraj</td>
+                        <td>Obriši</td>
+                    </tr></thead><tbody>';
+        while ($row=$artikli->fetch_assoc()){
+
+            echo"<tr><td>" . $row['Naziv'] . "</td>   
+                        <td >" . $row['Cijena'] . "</td>
+                        <td>" . $row['KolicinaZaliha'] . "</td>
+                        <td>" . $row['Opis'] . "</td>
+                        <td>" . $row['kategorijaNaziv'] . "</td>
+                        <td><a href='Artikli.php?artiklID=".$row['ID']."#formaZaRadArtiklima' name='azuriraj' id='".$row['ID']."' class='class=\"btn btn-warning btn-lg \"' role='button' >Ažuriraj</a></td>
+                        <td> <button name='delete' id='".$row['ID']."' class='class=\"btn btn-danger btn-lg \"' >Izbriši</button></td>
+                       
+                    
+                    </tr>";
+        }
+        echo"</tbody></table>";
+        $baza->zatvoriDB();
+    }else{
+        echo " <p class=\"h4 text-center\"> Tablica ne sadržava nikakve rezultate ! </p><hr><br>";
+    }
+
+}
+
 function DohvatiKategorije(){
     $i=0;
     $baza = new Baza();
