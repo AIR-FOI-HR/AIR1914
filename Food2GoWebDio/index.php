66fdf751a77931e4252992a880db1f83e225853a
igradiski <gradiski.zrinski.ivor@gmail.com>
2019-11-17 15:19:00 +0100

    Dodane skripte za izbornik i resetiranje lozinke korisnika #28

---
+++ Food2GoWebDio/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include("./Stranice/zaglavlje.php");
+include ("./Baza/Baza.php");
+
+$veza = new Baza();
+$veza->spojiDB();
+
+
+?>
+
+<h1>Ovo je početna stranica</h1>
+
+<?php
+
+include("./Stranice/podnozje.php");
+?>
+
