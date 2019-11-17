f1cfe56e1b97746e0dd9a4dce2c4b13e1b481410
igradiski <gradiski.zrinski.ivor@gmail.com>
2019-11-17 23:38:28 +0100

    Skripte za pregled povrantih informacija #37

---
+++ Food2GoWebDio/Stranice/PovratneInformacije.php
@@ -0,0 +1,17 @@
+<?php
+include("zaglavlje.php");
+include("../Backend/PovranteInformacijeBack.php");
+if(isset($_POST['nagrada'])){
+    DodajBodove();
+}
+
+
+
+?>
+<br>
+<div class="container h-100" >
+    <p class="h2 text-center">Povratne informacije</p><hr><br>
+    <?php
+    PrikaziPovratneInformacije();
+    ?>
+    <br>
