0a81cb9c56dc4d9a37166b5282ba47544d597d62
igradiski <gradiski.zrinski.ivor@gmail.com>
2019-12-03 19:29:28 +0100

    6.1 Server side Web Servisa Dodane skripte za dohvat korisnika koje se koriste kod registracije i prijave ============== Detaljni opis: - skripta htacces za rewrite rule - skripta dohvatiKorisnika vraca json file sa korisnicima - skripta funkcije koja vraća json zapise i razne rezultate ovisno o upitu -skripta registracijaKorisnika koja registrira novog korisnika i vraća json response ovisno o uspjehu registracije - skripta rest.php upravlja upitima koji dolaze s mobilne ili web aplikacije #41

---
+++ Food2GoWebDio/Servis/rest.php
@@ -0,0 +1,18 @@
+<?php
+include_once ('funkcije.php');
+header("Content-Type:application/json");
+
+if(isset($_GET)){
+    if(!empty($_GET['korisnici'])){
+        if($_GET['korisnici']=='svi'){
+            DohvatiSveKorisnike();
+        }
+    }
+    if(!empty($_GET['username']) && !empty($_GET['password'])  ){
+        DohvatiKorisnika($_GET['username'],$_GET['password']);
+    }
+
+}
+
+
+?>
