ec5208ee2d3ad28818b988b5e647aff9a7a7cf64
igradiski <gradiski.zrinski.ivor@gmail.com>
2019-12-05 18:42:52 +0100

    Dodane skripte za  aktivaciju korisnickog racuna i zaboravljenu lozinku ============== Detaljni opis: - .htacces dodani rewrite rule za aktivaciju koda i zaboravljenu lozinku - rest.php dodane putanje na skripte za aktivaciju i zaboravljenu lozinku - dodane funkcije za aktivaciju racuna, slanje aktivacijskog koda na mail i slanje lozinke na mail #41

--- Food2GoWebDio/Servis/funkcije.php
+++ Food2GoWebDio/Servis/funkcije.php
@@ -47,7 +47,7 @@ function DohvatiKorisnika($user,$pass){
     $status="OK";
     $red=[];
     $final=[];
-    $poruka="Korisinici su pronadjeni";
+    $poruka="Korisnici su pronadjeni";
     if ($brojRedova>0){
         while($row=$polje->fetch_assoc()){
             $red['id']=$row["ID"];
@@ -64,4 +64,67 @@ function DohvatiKorisnika($user,$pass){
     vratiOdgovor($status,$brojRedova,$poruka,$final);
 
 
-}
+}
+function posaljiKod($akt,$mail){
+    $kome=$mail;
+    $naslov="Aktivacijski kod";
+    $sadrzaj="Uspjesno ste registrirani, vas registracijski kod je ".$akt." i traje 24 sata";
+    $od="From: igradiski@foi.hr";
+    mail($kome,$naslov, $sadrzaj, $od);
+}
+function posaljiLozinkuMail($pass,$mail){
+    $kome=$mail;
+    $naslov="Lozinka";
+    $sadrzaj="Vaša lozinka je:".$pass ."";
+    $od="From: igradiski@foi.hr";
+    mail($kome,$naslov, $sadrzaj, $od);
+}
+function RegistrirajKorisnika($ime,$prezime,$username,$password,$oib,$email,$adresa,$brojmobitela,$brojpokusaja,$status,$aktivacijski,$bodovi,$tipkorisnika){
+    $postojiKorisnik=false;
+    $jsonArray = json_decode(DohvatiKorisnika($username,$password));
+    foreach ($jsonArray as $value){
+        if($value->Poruka=="Korisnici su pronadjeni"){
+            $postojiKorisnik=true;
+        }
+    }
+    if($postojiKorisnik==false){
+            //LUPI REGISTRACIJU SAD
+        $upit="INSERT INTO Korisnik values(DEFAULT,'$ime','$prezime','$username','$password','$oib','$email','$adresa','$brojmobitela','$brojpokusaja','0','$aktivacijski','$bodovi','$tipkorisnika' )";
+        izvrsiUpit($upit);
+        $status="OK";
+        $red=[];
+        $final=[];
+        $poruka="Korisnik je registriran";
+        posaljiKod($aktivacijski,$email);
+        vratiOdgovor($status,0,$poruka,$final);
+    }
+}
+
+function AktivirajKorisnika($mail,$aktivacijski){
+    $sql="UPDATE Korisnik set StatusKorisnika=1 WHERE  Email ='$mail' AND Aktivacijski_kod='$aktivacijski'";
+    izvrsiUpit($sql);
+    $status="OK";
+    $red=[];
+    $final=[];
+    $poruka="Korisnik je registriran";
+    vratiOdgovor($status,0,$poruka,$final);
+}
+function PosaljiLozinku($mail,$username) {
+    $polje=dohvatiRezultate("SELECT * FROM Korisnik WHERE KorisnickoIme='$username' AND Email = '$mail'");
+    $brojRedova=mysqli_num_rows($polje);
+    $status="OK";
+    $red=[];
+    $final=[];
+    $poruka="Mail s lozinkom je poslan";
+    if ($brojRedova>0){
+        while($row=$polje->fetch_assoc()){
+            posaljiLozinkuMail($row["Lozinka"],$row['Email']);
+        }
+    }
+    else{
+        $status="Nije OK";
+        $poruka="Ne postoji taj korisnik";
+
+    }
+    vratiOdgovor($status,$brojRedova,$poruka,$final);
+}
--- Food2GoWebDio/Servis/rest.php
+++ Food2GoWebDio/Servis/rest.php
@@ -11,6 +11,19 @@ if(isset($_GET)){
     if(!empty($_GET['username']) && !empty($_GET['password'])  ){
         DohvatiKorisnika($_GET['username'],$_GET['password']);
     }
+    if(!empty($_GET['ime']) && !empty($_GET['prezime']) && !empty($_GET['korisnickoime'])&& !empty($_GET['lozinka']) && !empty($_GET['oib'])&& !empty($_GET['email'])
+        && !empty($_GET['adresa']) && !empty($_GET['brojmobitela']) && !empty($_GET['brojpokusaja']) && !empty($_GET['status'])  && !empty($_GET['aktivacijski'])
+        && !empty($_GET['bodovi']) && !empty($_GET['tipkorisnika'])){
+        RegistrirajKorisnika(($_GET['ime']),($_GET['prezime']),($_GET['korisnickoime']),($_GET['lozinka']),($_GET['oib']),($_GET['email']),($_GET['adresa']),($_GET['brojmobitela']),($_GET['brojpokusaja']),($_GET['status']),($_GET['aktivacijski']),($_GET['bodovi']),($_GET['tipkorisnika']));
+    }
+
+    if(!empty($_GET['email']) && !empty($_GET['aktivacijski'])){
+        AktivirajKorisnika($_GET['email'],$_GET['aktivacijski']);
+    }
+    if(!empty($_GET['email']) && !empty($_GET['username'])){
+        PosaljiLozinku($_GET['email'],$_GET['username']);
+    }
+
 
 }
 
