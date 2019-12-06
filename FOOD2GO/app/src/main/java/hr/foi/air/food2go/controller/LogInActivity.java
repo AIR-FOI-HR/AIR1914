616a0137669d7a11b0ab4592c66dff74e58cd1c9
Natalija Jurčić <njurcic@foi.hr>
2019-12-07 00:26:06 +0100

    Prepravljena login aktivnost kako bi lakše bila implementirana zaboravljena lozinka. #4

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/controller/LogInActivity.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/controller/LogInActivity.java
@@ -1,5 +1,6 @@
 package hr.foi.air.food2go.controller;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,9 +22,6 @@ import hr.foi.air.food2go.controller.dataLoaders.WsDataLoader;
 public class LogInActivity extends AppCompatActivity implements DataLoadedListener {
 
     private EditText email, lozinka;
-    private Button btn_prijava;
-    private Button btn_zaboravljenaLozinka;
-    //private static String URL_LOGIN = "https://airfood2go.000webhostapp.com/Android/login.php";
 
     private WsDataLoader wsDataLoader;
 
@@ -32,30 +31,52 @@ public class LogInActivity extends AppCompatActivity implements DataLoadedListen
         setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
 
-        email = findViewById(R.id.korisnickoIme);
-        lozinka = findViewById(R.id.lozinka);
-        //btn_prijava = findViewById(R.id.uiActionPrijaviSe);
-       // btn_zaboravljenaLozinka = findViewById(R.id.uiActionZaboravljenaLozinka);
+        email = findViewById(R.id.korisnickoImePrijava);
+        lozinka = findViewById(R.id.lozinkaPrijava);
     }
 
     @OnClick(R.id.prijaviSe)
     public void KlikPrijava(View v) {
-        String korisnickoIme = email.getText().toString().trim();
-        String password = lozinka.getText().toString().trim();
+        if(Internet.isNetworkAvailable(this) == true) {
+            String korisnickoIme = email.getText().toString().trim();
+            String password = lozinka.getText().toString().trim();
 
-        Korisnik korisnik = new Korisnik(korisnickoIme, password);
-        wsDataLoader = new WsDataLoader();
-        wsDataLoader.Prijava(korisnik,this);
+            if(korisnickoIme.isEmpty() || password.isEmpty()){
+                AlertDialog alertDialog = new AlertDialog.Builder(LogInActivity.this).create();
+                alertDialog.setTitle("Nisu popunjeni svi podaci!");
+                alertDialog.setMessage("Molimo Vas unesite korisničko ime i lozinku!");
+                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+            else {
+                Korisnik korisnik = new Korisnik(korisnickoIme, password);
+                wsDataLoader = new WsDataLoader();
+                wsDataLoader.Prijava(korisnik, this);
+            }
+        }
+        else {
+            AlertDialog alertDialog = new AlertDialog.Builder(LogInActivity.this).create();
+            alertDialog.setTitle("Pogreška u internet vezi");
+            alertDialog.setMessage("Molimo Vas omogućite internetsku vezu kako bi ste se prijavili u aplikaciju.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
     }
 
-    /*
-    @OnClick(R.id.uiActionZaboravljenaLozinka)
+    @OnClick(R.id.zaboravljenaLozinka)
     public void KlikLozinka(View v) {
-        //String korisnickoIme = email.getText().toString().trim();
-
         startActivity(new Intent(LogInActivity.this, ZaboravljenaLozinkaActivity.class));
     }
-    */
 
     @Override
     public void onDataLoaded(String message, String status, Object data) {
