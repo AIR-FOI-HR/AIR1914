3fea21524bc2e927c0e6f7293380fba7006397c8
Jakov Nakić <jnakic@foi.hr>
2019-12-07 21:42:19 +0100

    U aktivnosti glavnog zaslona su kreirane funkcije za klik na kategoriju hranu, i za klik na kategoriju piće. #11

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniZaslonActivity.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniZaslonActivity.java
@@ -3,6 +3,12 @@ package hr.foi.air.food2go;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class GlavniZaslonActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class GlavniZaslonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glavni_zaslon);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.hranaKategorija)
+    private void KlikHrana(View v){
+        Toast.makeText(getApplicationContext(), "Odabrana kategorija: Hrana", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.piceKategorija)
+    private void KlikPice(View v){
+        Toast.makeText(getApplicationContext(), "Odabrana kategorija: Hrana", Toast.LENGTH_SHORT).show();
     }
 }
