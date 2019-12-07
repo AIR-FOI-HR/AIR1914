d3904a1f8c9be2cdc74dbb877b6ae9a7cc31c41d
Jakov Nakić <jnakic@foi.hr>
2019-12-07 16:04:15 +0100

    Kreiran je activity za glavni zaslon.

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniZaslonActivity.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniZaslonActivity.java
@@ -2,19 +2,13 @@ package hr.foi.air.food2go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class GlavniZaslonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_glavni_zaslon);
     }
 }
