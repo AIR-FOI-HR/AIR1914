722800ec98837cdafe6f9ef87f08185268350aed
Natalija Jurčić <njurcic@foi.hr>
2019-12-08 13:55:34 +0100

    Kreiran activity za aktivaciju koda. #3

---
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/controller/AktivacijskiKodActivity.java
@@ -0,0 +1,14 @@
+package hr.foi.air.food2go.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AktivacijskiKodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktivacijski_kod);
+    }
+}
