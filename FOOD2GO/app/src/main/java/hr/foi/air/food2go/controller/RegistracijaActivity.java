fa1d830fd67bfc86065c7d396a8919964ee54261
Monika Jakopanec <mjakopane@foi.hr>
2019-12-08 12:18:08 +0100

    Kreirana aktivnost za registraciju i napravljen dizajn za istu. #2

---
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/controller/RegistracijaActivity.java
@@ -0,0 +1,14 @@
+package hr.foi.air.food2go.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistracijaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registracija);
+    }
+}
