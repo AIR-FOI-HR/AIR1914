bd58d02ca372e176c3352742ac5752c33463c41e
Jakov Nakić <jnakic@foi.hr>
2019-12-11 21:05:49 +0100

    Kreiran je OdabirPotkategorijeActivity.

---
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/OdabirPotkategorijeActivity.java
@@ -0,0 +1,14 @@
+package hr.foi.air.food2go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OdabirPotkategorijeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_odabir_potkategorije);
+    }
+}
