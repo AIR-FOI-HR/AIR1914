93ac4e7901ec7bf13bc84414ab2b68207e283821
sosko123 <hsostaric@foi.hr>
2019-12-12 16:09:37 +0100

    U skriptu rest.php je dodan poziv funkcije  PromijeniKorisnickePodatke sa potrebnom validacijom kako bi uopće došlo do poziva funkcije #34

--- Food2GoWebDio/Servis/rest.php
+++ Food2GoWebDio/Servis/rest.php
@@ -1,59 +1,1 @@
-<?php
-include('funkcije.php');
-header("Content-Type:application/json");
-
-if (isset($_GET)) {
-    if (!empty($_GET['korisnici'])) {
-        if ($_GET['korisnici'] == 'svi') {
-            DohvatiSveKorisnike();
-        }
-    } elseif (!empty($_GET['username']) && !empty($_GET['password'])) {
-        DohvatiKorisnika($_GET['username'], $_GET['password']);
-    } elseif (isset($_GET['metoda'])) {
-        if($_GET["metoda"]=="reg"){
-            if (!empty($_GET['ime']) && !empty($_GET['prezime']) && !empty($_GET['korisnickoime']) && !empty($_GET['lozinka']) && !empty($_GET['oib']) && !empty($_GET['email'])) {
-                RegistrirajKorisnika(($_GET['ime']), ($_GET['prezime']), ($_GET['korisnickoime']), ($_GET['lozinka']), (($_GET['oib'])), ($_GET['email']), ($_GET['adresa']), ($_GET['brojmobitela']), "0", "0", ($_GET['aktivacijski']), "0", "2");
-            }
-        }
-        elseif ($_GET["metoda"]=="zaboravljena"){
-            if (!empty($_GET['email']) && !empty($_GET['username'])) {
-                PosaljiLozinku($_GET['email'], $_GET['username']);
-            }
-        }
-        elseif ($_GET["metoda"]=="kod"){
-            if (!empty($_GET['email']) && !empty($_GET['aktivacijski'])) {
-                AktivirajKorisnika($_GET['email'], $_GET['aktivacijski']);
-            }
-        }
-        elseif ($_GET["metoda"]=="narudbe"){
-            if(!empty($_GET['username'])){
-                DohvatiNarudzbe($_GET['username']);
-            }
-        }
-        elseif ($_GET["metoda"]=="blokiraj"){
-            if (!empty($_GET['username']) && !empty($_GET['blokiraj'])) {
-                BlokirajKorisnika($_GET['username'], $_GET['blokiraj']);
-            }
-        }
-        elseif($_GET["metoda"]=="kategorije" AND $_GET["kategorije"]=="sve"){
-            DohvatiSveKategorije();
-        }
-        elseif ($_GET["metoda"]=="artikli"){
-            if(!empty($_GET['kategorije'])){
-                DohvatiArtiklePoKategoriji($_GET['kategorije']);
-            }
-        }
-        elseif($_GET["metoda"]=="racun" AND $_GET["tip"]=="novi"){
-            if(!empty($_GET["korisnikid"])){
-                KreirajNoviRacun($_GET["korisnikid"]);
-            }
-
-        }
-        elseif ($_GET["metoda"]=="dodajnarudbi"){
-            if(!empty($_GET["artiklid"])){
-                DohvatiArtikl($_GET["artiklid"]);
-            }
-        }
-    }
-
-}
+<?phpinclude('funkcije.php');header("Content-Type:application/json");if (isset($_GET)) {    if (!empty($_GET['korisnici'])) {        if ($_GET['korisnici'] == 'svi') {            DohvatiSveKorisnike();        }    } elseif (!empty($_GET['username']) && !empty($_GET['password'])) {        DohvatiKorisnika($_GET['username'], $_GET['password']);    } elseif (isset($_GET['metoda'])) {        if($_GET["metoda"]=="reg"){            if (!empty($_GET['ime']) && !empty($_GET['prezime']) && !empty($_GET['korisnickoime']) && !empty($_GET['lozinka']) && !empty($_GET['oib']) && !empty($_GET['email'])) {                RegistrirajKorisnika(($_GET['ime']), ($_GET['prezime']), ($_GET['korisnickoime']), ($_GET['lozinka']), (($_GET['oib'])), ($_GET['email']), ($_GET['adresa']), ($_GET['brojmobitela']), "0", "0", ($_GET['aktivacijski']), "0", "2");            }        }        elseif ($_GET["metoda"]=="zaboravljena"){            if (!empty($_GET['email']) && !empty($_GET['username'])) {                PosaljiLozinku($_GET['email'], $_GET['username']);            }        }        elseif ($_GET["metoda"]=="kod"){            if (!empty($_GET['email']) && !empty($_GET['aktivacijski'])) {                AktivirajKorisnika($_GET['email'], $_GET['aktivacijski']);            }        }        elseif ($_GET["metoda"]=="narudbe"){            if(!empty($_GET['username'])){                DohvatiNarudzbe($_GET['username']);            }        }        elseif ($_GET["metoda"]=="blokiraj"){            if (!empty($_GET['username']) && !empty($_GET['blokiraj'])) {                BlokirajKorisnika($_GET['username'], $_GET['blokiraj']);            }        }        elseif($_GET["metoda"]=="kategorije" AND $_GET["kategorije"]=="sve"){            DohvatiSveKategorije();        }        elseif ($_GET["metoda"]=="artikli"){            if(!empty($_GET['kategorije'])){                DohvatiArtiklePoKategoriji($_GET['kategorije']);            }        }        elseif($_GET["metoda"]=="racun" AND $_GET["tip"]=="novi"){            if(!empty($_GET["korisnikid"])){                KreirajNoviRacun($_GET["korisnikid"]);            }        }        elseif ($_GET["metoda"]=="dodajnarudbi"){            if(!empty($_GET["artiklid"])){                DohvatiArtikl($_GET["artiklid"]);            }        }        elseif ($_GET["metoda"]=="azurirajKorisnika"){            if(!empty($_GET["ime"]) && !empty($_GET["prezime"]) && !empty($_GET["username"]) && !empty($_GET["lozinka"]) && !empty($_GET["id"]) && !empty($_GET["email"])){                PromijeniKorisnickePodatke(($_GET["ime"]),($_GET["prezime"]),($_GET["username"]),($_GET["adresa"]),($_GET["lozinka"]),($_GET["mobitel"]),($_GET['id']),($_GET["email"]));            }        }    }}
