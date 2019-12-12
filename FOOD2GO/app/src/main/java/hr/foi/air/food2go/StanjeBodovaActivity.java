b64599c414863f7f7f355d45bd50b047c8c4b923
Jakov Nakić <jnakic@foi.hr>
2019-12-12 17:20:31 +0100

    Kreiran je StanjeBodovaActivity. #7

---
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/StanjeBodovaActivity.java
@@ -0,0 +1,14 @@
+package hr.foi.air.food2go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StanjeBodovaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stanje_bodova);
+    }
+}
