70974a4982f6129b90afaa1999e1689db23e8f90
Natalija Jurčić <njurcic@foi.hr>
2019-12-13 21:57:31 +0100

    Promjenjen import. #8

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniActivity.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/GlavniActivity.java
@@ -21,7 +21,7 @@ import com.google.android.material.navigation.NavigationView;
 import hr.foi.air.food2go.controller.Internet;
 import hr.foi.air.food2go.controller.LogInActivity;
 import hr.foi.air.food2go.fragmenti.kategorije.KategorijeViewModel;
-import hr.foi.air.food2go.fragmenti.moje_narudzbe.MojeNarudzbeViewModel;
+import hr.foi.air.food2go.fragmenti.moje_narudzbe.MojeNarudzbeFragment;
 import hr.foi.air.food2go.fragmenti.nagrade.NagradeViewModel;
 import hr.foi.air.food2go.fragmenti.postavke.PostavkeViewModel;
 import hr.foi.air.food2go.fragmenti.stanje_bodova.StanjeBodovaViewModel;
