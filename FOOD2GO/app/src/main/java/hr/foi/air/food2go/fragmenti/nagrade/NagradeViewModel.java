1204d53624d3ffde7cd2146435c2da6d75896a89
Jakov Nakić <jnakic@foi.hr>
2019-12-16 19:50:10 +0100

    Kreiran je dizajn za Nagrade, i napravljen je layout za jedan element nagrade koji će biti prikazan u recyclerview-u. #14

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/fragmenti/nagrade/NagradeViewModel.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/fragmenti/nagrade/NagradeViewModel.java
@@ -9,13 +9,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import hr.foi.air.food2go.R;
+import java.util.ArrayList;
 
-public class NagradeViewModel extends Fragment {
+import hr.foi.air.food2go.R;
+import hr.foi.air.food2go.controller.dataLoaders.DataLoadedListener;
+import hr.foi.air.food2go.controller.dataLoaders.WsDataLoader;
+import hr.foi.air.food2go.model.Nagrada;
+
+public class NagradeViewModel extends Fragment implements DataLoadedListener {
+
+    View view;
+    private WsDataLoader wsDataLoader;
+    private ArrayList<Nagrada> nagrade;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = container;
         return inflater.inflate(R.layout.fragment_nagrade, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        nagrade = new ArrayList<>();
+        wsDataLoader = new WsDataLoader();
+    }
+
+    @Override
+    public void onDataLoaded(String message, String status, Object data) {
+
     }
 }
