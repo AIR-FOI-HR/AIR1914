b7c266d3bd87232a64e106198ad3f0b319d70274
Monika Jakopanec <mjakopane@foi.hr>
2020-01-26 23:10:40 +0100

    U fragment TrenutnaNarudzba su dodane metode potrebne za plaćanje preko Braintree sustava. Klikom na gumb "Naruči" otvara se drop-in design na kojem je moguće odabrati plaćanje PayPal-om ili drugim debitnim karticama. Odabirom jednog od ponuđenih načina plaćanja, otvara se prozor za unos broja debitne kartice i datum valjanosti iste. Nakon unosa podataka, klikom na ADD CARD iz tog prozora, vrši se transakcija (ukoliko je kartica valjana). #59

--- FOOD2GO/app/src/main/java/hr/foi/air/food2go/fragmenti/trenutna_narudzba/TrenutnaNarudzbaFragment.java
+++ FOOD2GO/app/src/main/java/hr/foi/air/food2go/fragmenti/trenutna_narudzba/TrenutnaNarudzbaFragment.java
@@ -2,8 +2,11 @@ package hr.foi.air.food2go.fragmenti.trenutna_narudzba;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -19,8 +22,24 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.dropin.DropInActivity;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.interfaces.HttpResponseCallback;
+import com.braintreepayments.api.internal.HttpClient;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +49,7 @@ import hr.foi.air.core.BodoviVjernostiView;
 import hr.foi.air.core.Korisnik;
 import hr.foi.air.core.Racun;
 import hr.foi.air.food2go.GlavniActivity;
+import hr.foi.air.food2go.MainActivity;
 import hr.foi.air.food2go.controller.Internet;
 import hr.foi.air.food2go.controller.dataLoaders.DataLoadedListener;
 import hr.foi.air.food2go.controller.dataLoaders.WsDataLoader;
@@ -38,8 +58,16 @@ import hr.foi.air.food2go.recyclerview.TrenutnaNarudzbaRecyclerAdapter;
 
 import hr.foi.air.food2go.R;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedListener, View.OnClickListener {
 
+    private static final int REQUEST_CODE = 1234;
+    final String API_GET_TOKEN = "https://airfood2go.000webhostapp.com/Servis/Braintree/main.php";
+    final String API_CHECK_OUT = "https://airfood2go.000webhostapp.com/Servis/Braintree/checkout.php";
+    String token;
+    HashMap <String, String> paramsHash;
 
     private ArrayList<Artikl> artikliNarudzbe = new ArrayList<>();
     private WsDataLoader wsDataLoader;
@@ -86,6 +114,8 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
                 dodaneStavke = false;
                 DohvatiIzgled();
                 wsDataLoader.DohvatiArtiklePoKategoriji(this, "1");
+
+                new getToken().execute();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -102,10 +132,127 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
                     });
             alertDialog.show();
         }
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce nonce = result.getPaymentMethodNonce();
+                String strNonce = nonce.getNonce();
+                if(!ukupno.getText().toString().isEmpty()){
+                    Toast.makeText(getActivity(), "Trenutak molim...", Toast.LENGTH_SHORT).show();
+                    paramsHash = new HashMap<>();
+                    paramsHash.put("amount", ukupno.getText().toString());
+                    paramsHash.put("nonce", strNonce);
 
+                    sendPayments();
+                }
+                else{
+                    Toast.makeText(getActivity(),"Molimo Vas odaberite artikle.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(resultCode == RESULT_CANCELED)
+                Toast.makeText(getActivity(), "Otkazivanje...", Toast.LENGTH_SHORT).show();
+            else{
+                Exception error = (Exception)data.getSerializableExtra(DropInActivity.EXTRA_ERROR);
+                Log.d("EDMT_ERROR", error.toString());
+            }
+        }
+    }
 
+    private void sendPayments() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, API_CHECK_OUT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.toString().toLowerCase().contains("successful")){
+                            if (iskoristenPopust == true) {
+                                wsDataLoader.ZabiljeziBodoveVjernosti(Korisnik.getPrijavljeniKorisnik(), bodoviVjernostiView);
+                            }
+                            wsDataLoader.KreirajRacun(Korisnik.getPrijavljeniKorisnik());
+                            if (racun != null) {
+                                dodajArtikleNaRacun(racun);
+                            }
+                        }
+                        else{
+                            Toast.makeText(getActivity(), "Nesupješna transakcija", Toast.LENGTH_SHORT).show();
+                        }
+                        Log.d("EDMT_LOG", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("EDMT_ERROR", error.toString());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                if(paramsHash == null)
+                    return null;
+                Map<String, String> params = new HashMap<>();
+                for(String key:paramsHash.keySet())
+                {
+                    params.put(key,paramsHash.get(key));
+                }
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        queue.add(stringRequest);
+    }
+
+    private class getToken extends AsyncTask{
+
+        ProgressDialog mDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mDialog = new ProgressDialog(getActivity(), android.R.style.Theme_DeviceDefault_Dialog);
+            mDialog.setCancelable(false);
+            mDialog.setMessage("Trenutak molim!");
+            mDialog.show();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            HttpClient client = new HttpClient();
+            client.get(API_GET_TOKEN, new HttpResponseCallback() {
+                @Override
+                public void success(final String responseBody) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            token = responseBody;
+                        }
+                    });
+                }
+
+                @Override
+                public void failure(Exception exception) {
+                    Log.d("EDMT_ERROR", exception.toString());
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            mDialog.dismiss();
+        }
     }
 
     @Override
@@ -114,6 +261,7 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
         if (entityType == BodoviVjernostiView.class  ) {
             if(status.equals("OK")){
                 uracunajPopust(status, (BodoviVjernostiView) data);
+
             }else {
                 Toast.makeText(getContext(),"Nemate pravo na popust !",Toast.LENGTH_SHORT).show();
             }
@@ -127,15 +275,12 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
 
         if (entityType == Racun.class && dodaneStavke == false) {
             dodajArtikleNaRacun((Racun) data);
-
-
         }
     }
 
     private void getSharedPrefs() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
     }
-
 
     private void DohvatiIzgled() {
         RecyclerView recyclerView = v.findViewById(R.id.trenutna_narudzbaRecycler);
@@ -201,18 +346,18 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
             if(artikliNarudzbe.size()==0){
                 Toast.makeText(getContext(),"Košarica je prazna !!!",Toast.LENGTH_SHORT).show();
             }else{
-                if (iskoristenPopust == true) {
-                    wsDataLoader.ZabiljeziBodoveVjernosti(Korisnik.getPrijavljeniKorisnik(), bodoviVjernostiView);
-                }
-                wsDataLoader.KreirajRacun(Korisnik.getPrijavljeniKorisnik());
-                if (racun != null) {
-                    dodajArtikleNaRacun(racun);
-                }
+                submitPayment();
             }
-            }
-             else {
+        }
+        else {
             Toast.makeText(getContext(), "Nema interneta", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    private void submitPayment() {
+        DropInRequest dropInRequest = new DropInRequest().clientToken(token);
+        startActivityForResult(dropInRequest.getIntent(getActivity()), REQUEST_CODE);
 
     }
 
@@ -230,9 +375,9 @@ public class TrenutnaNarudzbaFragment extends Fragment implements DataLoadedList
         alert.setTitle("Uspjeh !");
         alert.setMessage("Artikli su dodani na racun !");
         alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+           @Override
+           public void onClick(DialogInterface dialog, int which) {
+                 dialog.dismiss();
             }
         });
         alert.show();
